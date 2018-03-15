e7dce871b776a41f9c1dd671dd66c33187f8c325
qsmsf <qsmsf@sohu.com>
2018-03-15 12:16:45 +0800

    营业分析接口

--- Biz/BizBusinessAnalysis.cs
+++ Biz/BizBusinessAnalysis.cs
@@ -11,13 +11,42 @@ namespace DianDianClient.Biz
     class BizBusinessAnalysis
     {
         log4net.ILog log = log4net.LogManager.GetLogger("BizBusinessAnalysis");
-        private DianDianEntities db = new DianDianEntities();
+        //private DianDianEntities db = new DianDianEntities();
 
+        public class StatisticBean{
+            public string name { get; set; }
+            public int cnt { get; set; }
+            public decimal sum { get; set; }
+        }
+
+        public class StatisticViewResult
+        {
+            public int usernum { get; set; }
+            public string type { get; set; }
+            public int itemnum { get; set; }
+            public int itemkey { get; set; }
+            public string itemname { get; set; }
+        }
+
+        public class StatisticMonthBean
+        {
+            public string name { get; set; }
+            public int month { get; set; }
+            public decimal summoney { get; set; }
+        }
+
+        public class StatisticMonthResult
+        {
+            public decimal summoney { get; set; }
+            public int month { get; set; }
+            public int type { get; set; }
+        }
         //查询营业额
       public decimal QueryTotleTurnover(DateTime sdate, DateTime edate)
         {
             try
             {
+                DianDianEntities db = new DianDianEntities();
                 decimal rsl = db.dd_shop_account.Where(p => p.shopkey == Properties.Settings.Default.shopkey
                     && p.createdate >= sdate && p.createdate <= edate).Sum(p => p.money - p.brokerage).Value;
 
@@ -34,6 +63,7 @@ namespace DianDianClient.Biz
         {
             try
             {
+                DianDianEntities db = new DianDianEntities();
                 return db.dd_shop_account.Where(p => p.shopkey == Properties.Settings.Default.shopkey
                     && p.createdate >= sdate && p.createdate <= edate).Count();
             }
@@ -54,7 +84,7 @@ namespace DianDianClient.Biz
 
                 //List<dd_shop_account> recordList = db.dd_shop_account.Where(p => p.shopkey == Properties.Settings.Default.shopkey
                 //&& SqlFunctions.DateAdd("dd", 0, p.createdate) <= edate).ToList();
-
+                DianDianEntities db = new DianDianEntities();
                 List<v_cfmainaccount> recordList = (from a in db.v_cfmainaccount
                                                     where a.shopkey == Properties.Settings.Default.shopkey
                                                     &&
@@ -135,6 +165,7 @@ namespace DianDianClient.Biz
         {
             try
             {
+                DianDianEntities db = new DianDianEntities();
                 List<v_cfmainaccount> recordList = db.v_cfmainaccount.Where(p => p.shopkey == Properties.Settings.Default.shopkey
                     && p.createdate >= sdate && p.createdate <= edate).ToList();
                 var rslList = recordList.GroupBy(p => p.paytype);
@@ -166,6 +197,7 @@ namespace DianDianClient.Biz
         {
             try
             {
+                DianDianEntities db = new DianDianEntities();
                 List<RecordGroupTotleBean> rslbeanList = new List<RecordGroupTotleBean>();
                 List<v_cfmainaccount> recordList = db.v_cfmainaccount.Where(p => p.shopkey == Properties.Settings.Default.shopkey
                     && p.createdate >= sdate && p.createdate <= edate).ToList();
@@ -226,6 +258,119 @@ namespace DianDianClient.Biz
             }
 
             return name;
+        }
+
+        public List<StatisticBean> getStatisticInfo(DateTime? sdate, DateTime? edate)
+        {
+            try
+            {
+                DianDianEntities db = new DianDianEntities();
+                List<StatisticBean> statisticList = new List<StatisticBean>();
+
+                int shopkey = Properties.Settings.Default.shopkey;
+                string datesql = "";
+                if(sdate!= null)
+                {
+                    datesql += " and a.createdate >= '"+sdate.Value.ToString("yyyy-MM-dd")+" 00:00:00' ";
+                }
+                if(edate != null)
+                {
+                    datesql += " and a.createdate <= '" + edate.Value.ToString("yyyy-MM-dd") + " 23:59:59' ";
+                }
+                string sql = "SELECT  x.count usernum,x.type,y.count itemnum,y.itemkey,y.itemname  FROM ( "
+                + "	SELECT COUNT(1) 'count',TYPE FROM v_dd_count_mid a WHERE a.shopkey = " + shopkey + datesql + "  GROUP BY a.type)X "
+                + "LEFT JOIN "
+                + "( "
+                + "	(SELECT COUNT(1) 'count',a.* FROM v_dd_count_mid_2 a WHERE a.type=1 AND a.shopkey = " + shopkey + datesql + " GROUP BY a.itemkey ORDER BY COUNT(1) DESC LIMIT 0,3) "
+                + "	UNION ALL "
+                + "	(SELECT COUNT(1) 'count',a.* FROM v_dd_count_mid_2 a WHERE a.type=2 AND a.shopkey = " + shopkey + datesql + " GROUP BY a.itemkey ORDER BY COUNT(1) DESC LIMIT 0,3) "
+                + "	UNION ALL "
+                + "	(SELECT COUNT(1) 'count',a.* FROM v_dd_count_mid_2 a WHERE a.type=3 AND a.shopkey = " + shopkey + datesql + " GROUP BY a.itemkey ORDER BY COUNT(1) DESC LIMIT 0,3) "
+                + ") AS Y ON y.type=x.type ";
+
+                var resultList = db.Database.SqlQuery<StatisticViewResult>(sql);
+                string flag = "0";
+                foreach(var result in resultList)
+                {
+                    StatisticBean bean1 = new StatisticBean();
+                    if (!flag.Equals(result.type))
+                    {
+                        bean1.cnt = result.usernum;
+                        bean1.sum = result.itemnum;
+                        if ("1".Equals(result.type))
+                        {
+                            bean1.name = "新顾客";
+                        }else if ("2".Equals(result.type))
+                        {
+                            bean1.name = "老顾客";
+                        }
+                        else if ("3".Equals(result.type))
+                        {
+                            bean1.name = "会员";
+                        }
+                        statisticList.Add(bean1);
+                    }
+                }
+                return statisticList;
+            }
+            catch(Exception e)
+            {
+                log.Error("getStatisticInfo error! msg="+e.Message);
+                throw;
+            }
+        }
+
+        public List<StatisticMonthResult> getStatisticLineInfo(int year)
+        {
+            try
+            {
+                DianDianEntities db = new DianDianEntities();
+                List<StatisticMonthResult> statisticList = new List<StatisticMonthResult>();
+                int shopkey = Properties.Settings.Default.shopkey;
+
+                string sql = "select sum(a.summoney) as summoney,month(a.createdate) month,a.type from ( "
+                + "	select sum(mn.realpay) as summoney,"
+                + "	  `mr`.`memberkey`  AS `userid`,"
+                + "	  `ml`.`shopkey`    AS `shopkey`,"
+                + "	  `ml`.`createDate` AS `createdate`,"
+                + "	  (case "
+                + "		when ((count(1) = 1) and isnull(`d`.`userid`)) then 1"
+                + "		when ((count(1) > 1) and isnull(`d`.`userid`)) then 2 "
+                + "		when (`d`.`userid` is not null) then 3 "
+                + "	   end) AS `type`"
+                + "	from ((`cf_member` `mr`"
+                + "	    left join `cf_meal` `ml`"
+                + "	      on ((`ml`.`cfmealkey` = `mr`.`cfmealkey`) )) "
+                + "	    left join `dd_mem_card` `d`"
+                + "	      on ((`d`.`userid` = `mr`.`memberkey`)AND UNIX_TIMESTAMP(ml.createDate)>=d.addtime))"
+                + "	    left join cf_main mn on mn.cfmealkey=ml.cfmealkey "
+                + "	where mn.realpay IS NOT NULL and year(a.createdate) = " + year + " and a.shopkey = " + shopkey
+                  + "	group by mr.memberkey,d.addtime  "
+                + ")a "                
+                + "group by a.type, month(a.createdate) "
+                + "order by a.type, month(a.createdate) ";
+
+                statisticList = db.Database.SqlQuery<StatisticMonthResult>(sql).ToList();
+
+                return statisticList;
+            }
+            catch(Exception e)
+            {
+                log.Error("getStatisticRechageInfo error! msg=" + e.Message);
+                throw;
+            }
+        }
+
+        public void getStatisticGlabolInfo()
+        {
+            try
+            {
+
+            }catch(Exception e)
+            {
+                log.Error("getStatisticGlabolInfo error! msg=" + e.Message);
+                throw;
+            }
         }
     }
 }
